794c7e8ed3a1b14578077dd4d97e6a661bf05878
KSDragon82 <www.kiarashs2929@gmail.com>
2020-10-14 22:59:26 +0330

    changed api url

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
 
 export default axios.create({
-	baseURL: 'http://localhost:3001/api' //'https://realestate-server-amin.herokuapp.com/api'
+	baseURL: 'https://realestate-server-amin.herokuapp.com/api'
 });
